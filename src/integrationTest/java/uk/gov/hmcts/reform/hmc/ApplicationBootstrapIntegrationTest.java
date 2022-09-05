0b9bdae32af6be54cd8dfb68634c4710de8724cd
rebeccabaker <rebecca.baker@hmcts.net>
2022-09-05 11:21:49 +0100

    HMAN-387 added integration test folder

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/ApplicationBootstrapIntegrationTest.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.hmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {ApplicationRunner.class})
+class ApplicationBootstrapIntegrationTest {
+    private static final String FLAG = "true";
+
+    @Test
+    void testShouldBootstrapSpringContext() {
+        assertThat(Boolean.valueOf(FLAG)).isTrue();
+    }
+}
+
