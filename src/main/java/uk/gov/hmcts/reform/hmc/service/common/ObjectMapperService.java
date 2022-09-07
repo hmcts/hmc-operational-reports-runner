d3ce79e1d410b0f51ae2945fb834878cb2bb153d
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-09-07 11:43:33 +0100

    HMAN-389- Removing unnecessary RoleAssignment files

--- src/main/java/uk/gov/hmcts/reform/hmc/service/common/ObjectMapperService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.hmc.service.common;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface ObjectMapperService {
-
-    JsonNode convertObjectToJsonNode(Object object);
-
-}
