65b61b90bb266c48f0ebf77e55c9a08df4742149
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-09-07 10:54:10 +0100

    HMAN-389- Removing unnecessary enum files

--- src/main/java/uk/gov/hmcts/reform/hmc/domain/model/enums/ActorIdType.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.hmc.domain.model.enums;
-
-// mirrors: uk.gov.hmcts.reform.roleassignment.domain.model.enums.ActorIdType
-public enum ActorIdType {
-    IDAM,
-    CASEPARTY
-}
