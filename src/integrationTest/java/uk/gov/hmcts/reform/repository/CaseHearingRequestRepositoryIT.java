0a4174843441fab9e85bc867cfc4798af39acf72
markdathorne <mark.dathorne@cgi.com>
2022-09-08 17:41:15 +0100

    HMAN-389 Code coverage tweak

--- src/integrationTest/java/uk/gov/hmcts/reform/repository/CaseHearingRequestRepositoryIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/repository/CaseHearingRequestRepositoryIT.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.hmc.ApplicationParams;
+import uk.gov.hmcts.reform.hmc.BaseTest;
 import uk.gov.hmcts.reform.hmc.data.CaseHearingRequestEntity;
 import uk.gov.hmcts.reform.hmc.repository.CaseHearingRequestRepository;
 import uk.gov.hmcts.reform.hmc.service.NotifyService;
