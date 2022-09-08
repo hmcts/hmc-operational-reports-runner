39bd4cc2fe004f5ea9fe8a9bd075264826ae5bef
rebeccabaker <rebecca.baker@hmcts.net>
2022-09-08 15:58:07 +0100

    HMAN-389 Tidy up files

--- src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/constants/Constants.java
@@ -2,11 +2,8 @@ package uk.gov.hmcts.reform.hmc.constants;
 
 public final class Constants {
 
-    public static final String POST_HEARING_STATUS = "HEARING_REQUESTED";
     public static final String UNAVAILABILITY_RANGE_TYPE = "Range";
     public static final String UNAVAILABILITY_DOW_TYPE = "DOW";
-    public static final String CANCELLATION_REQUESTED = "CANCELLATION_REQUESTED";
-    public static final Integer DURATION_OF_DAY = 360;
 
     private Constants() {
     }
