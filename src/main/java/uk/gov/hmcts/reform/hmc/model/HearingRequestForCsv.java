a5a5607f496fa3090fcbee9125bdf41b546483be
markdathorne <mark.dathorne@cgi.com>
2022-09-13 14:18:06 +0100

    HMAN-390 Add spaced column titles for CSV header

--- src/main/java/uk/gov/hmcts/reform/hmc/model/HearingRequestForCsv.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/model/HearingRequestForCsv.java
@@ -1,17 +1,26 @@
 package uk.gov.hmcts.reform.hmc.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class HearingRequestForCsv {
+    @JsonProperty("Case Reference")
     private String caseReference;
+    @JsonProperty("Case Name")
     private String caseName;
+    @JsonProperty("Hearing Status")
     private String hearingStatus;
+    @JsonProperty("Hearing Id")
     private String hearingId;
+    @JsonProperty("List Assist Id")
     private String listAssistId;
+    @JsonProperty("Hearing Request Received Date Time")
     private String hearingRequestReceivedDateTime;
+    @JsonProperty("Hearing Response Received Date Time")
     private String hearingResponseReceivedDateTime;
+    @JsonProperty("First Scheduled Hearing Date Time")
     private String firstScheduledHearingDate;
 }
