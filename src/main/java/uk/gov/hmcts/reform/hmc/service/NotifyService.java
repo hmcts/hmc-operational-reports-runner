7e84a1bd6b098a3fb31484c15f44dcf82474e692
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-02-25 11:09:38 +0000

    HMAN-1094 -Revert all changes and add nvd API key

--- src/main/java/uk/gov/hmcts/reform/hmc/service/NotifyService.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/service/NotifyService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.hmc.service;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -57,7 +58,7 @@ public class NotifyService {
                 templateId,
                 emailAddress,
                 personalisation,
-                "",
+                StringUtils.EMPTY,
                 replyToEmailAddress
             ));
         }
